482780298ea2e092fc3be81a809733b530e046ee
Doug Rabson <dfr@rabson.org>
2023-07-27 11:37:34 +0100

    WIP: device support for freebsd
    Signed-off-by: Doug Rabson <dfr@rabson.org>

--- internal/factory/container/device_freebsd.go
+++ internal/factory/container/device_freebsd.go
@@ -5,5 +5,54 @@ import (
 )
 
 func (c *container) SpecAddDevices(configuredDevices, annotationDevices []devicecfg.Device, privilegedWithoutHostDevices, enableDeviceOwnershipFromSecurityContext bool) error {
+	// TODO: implement internal/config/device for FreeBSD and add code here to modify the devfs mount accordingly.
+
+	// After that, add additional_devices from config
+	//for i := range configuredDevices {
+	//	d := &configuredDevices[i]
+	//	c.specAddDevice(d.Device)
+	//}
+
+	// Next, verify and add the devices from annotations
+	//for i := range annotationDevices {
+	//	d := &annotationDevices[i]
+	//	c.specAddDevice(d.Device)
+	//}
+
+	// Then, add host devices if privileged
+	if err := c.specAddHostDevicesIfPrivileged(privilegedWithoutHostDevices); err != nil {
+		return err
+	}
+
+	// Then, add container config devices
+	if err := c.specAddContainerConfigDevices(enableDeviceOwnershipFromSecurityContext); err != nil {
+		return err
+	}
+
+	// Finally, inject CDI devices
+	return c.specInjectCDIDevices()
+}
+
+func (c *container) specAddHostDevicesIfPrivileged(privilegedWithoutHostDevices bool) error {
+	if !c.Privileged() || privilegedWithoutHostDevices {
+		return nil
+	}
+
+	// Modify the devfs mount to add all devices
+	for i, m := range c.Spec().Config.Mounts {
+		if m.Type == "devfs" {
+			m.Options = []string{"ruleset=0"}
+			c.Spec().Config.Mounts[i] = m
+			return nil
+		}
+	}
+	return nil
+}
+
+func (c *container) specAddContainerConfigDevices(enableDeviceOwnershipFromSecurityContext bool) error {
+	return nil
+}
+
+func (c *container) specInjectCDIDevices() error {
 	return nil
 }
